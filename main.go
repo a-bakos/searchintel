77b57961eca32b8032cfde821007c73ab1204949
a-bakos <bakosattila@fastem.com>
2024-08-28 21:20:45 +0100

    Add CSV test sheet + simple file reader

--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
 )
 
+type SearchEntry struct {
+	Id    int32
+	Time  string
+	Query string
+	Url   string
+	Hits  int32
+	//user   string
+	//email  string
+	Target string
+	//ip     string
+}
+
 func main() {
-	fmt.Println("Hi")
+	//testSearch := SearchEntry{1, "2024/03/23 19:06:04", "cats", "http://example.com/home", 2, "http://example.com/kittens"}
+
+	file, err := os.Open("testsheet.csv")
+	if err != nil {
+		panic("Error")
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		panic("Error")
+	}
+
+	fmt.Println(records)
 }
