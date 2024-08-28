10a278e61a22a7e551747d982e4f76329e3c59b2
a-bakos <bakosattila@fastem.com>
2024-08-28 22:23:37 +0100

    Parse CSV rows into custom type

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type SearchEntry struct {
@@ -21,6 +22,8 @@ type SearchEntry struct {
 func main() {
 	//testSearch := SearchEntry{1, "2024/03/23 19:06:04", "cats", "http://example.com/home", 2, "http://example.com/kittens"}
 
+	var searchEntries []SearchEntry
+
 	file, err := os.Open("testsheet.csv")
 	if err != nil {
 		panic("Error")
@@ -35,4 +38,33 @@ func main() {
 	}
 
 	fmt.Println(records)
+
+	for i, line := range records {
+		if i == 0 { // skip first row
+			continue
+		}
+		if err != nil {
+			panic("Error")
+		}
+
+		// Parse string as int32
+		convert := func(theString string) int32 {
+			idInt, err := strconv.ParseInt(theString, 10, 64)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			return int32(idInt)
+		}
+
+		searchEntries = append(searchEntries, SearchEntry{
+			convert(line[0]),
+			line[1],
+			line[2],
+			line[3],
+			convert(line[4]),
+			line[7]})
+	}
+
+	fmt.Println(searchEntries)
 }
